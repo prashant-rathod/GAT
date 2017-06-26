f7d54aa7543052d097f40525edbc09f3f4d4ac1d
Nikita Zemlevskiy <nikita.zemlevskiy@consumer.org>
2017-06-26 15:15:41 -0400

    update and add lifecycle scripts

--- lifecycle/clean.sh
+++ lifecycle/clean.sh
@@ -4,6 +4,9 @@ echo "CLEAN:"
 echo "REMOVING .pyc FILES"
 find .. -name \*.pyc -delete
 
+echo "REMOVING .bak FILES"
+find .. -name \*.bak -delete
+
 echo "REMOVING __pycache__ DIRS"
 find .. -name __pycache__ -type d -delete
 
