179bfa6065e54443d47045a1e9b119c84eebe83a
rsteed11 <rsteed11@gmail.com>
2017-08-10 13:35:28 -0400

    Added influence propensities

--- SNA.py
+++ SNA.py
@@ -222,43 +222,103 @@ class SNA():
                         #####################################
                         ### Propensity assignment section ###
                         #####################################
-                        ## Emotion Propensities
+
+                        ## Emotion & Influence Propensities ##
+
                         src_w = float(src_val[1]["W"]) if "W" in src_val[1] else None
                         trg_w = float(trg_val[1]["W"]) if "W" in trg_val[1] else None
 
                         if src_w is not None and trg_w is not None:
-                            # Cooperative propensities
+                            # Cooperative propensities:
                             if src_val[0] == trg_val[0]:
                                 # Checking to see if each node's attribute weights fall within specified ranges:
                                 if src_w >= 0.6 and trg_w >= 0.6:
-                                    emoProps.append(("Trust",attr,src_val[0],trg_val[0],src_w,trg_w))
+                                    emoProps.append(("Trust",attr,src_val[0],trg_val[0],src_w,trg_w,src_w*trg_w))
+                                    inflProps += [
+                                        ("Reciprocation",attr,src_val[0],trg_val[0],src_w,trg_w,0.8),
+                                        ("Commitment & Consistency", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                        ("Social Proof", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                        ("Liking", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                        ("Authority", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                        ("Scarcity", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                    ]
                                 else: # all others are joy
-                                    emoProps.append(("Joy", attr, src_val[0], trg_val[0],src_w,trg_w))
-                                    # print("Appended Joy using attribute", attr, "(", src_val, "&", trg_val, ")",
-                                    #       "for node pair (", source, ",", target, ")")
+                                    emoProps.append(("Joy", attr, src_val[0], trg_val[0],src_w,trg_w,src_w*trg_w))
+                                    inflProps += [
+                                        ("Reciprocation", attr, src_val[0], trg_val[0], src_w, trg_w, 0.5),
+                                        ("Commitment & Consistency", attr, src_val[0], trg_val[0], src_w, trg_w, 0.5),
+                                        ("Social Proof", attr, src_val[0], trg_val[0], src_w, trg_w, 0.7),
+                                        ("Liking", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                        ("Authority", attr, src_val[0], trg_val[0], src_w, trg_w, 0.5),
+                                    ]
 
                             elif attr in emoAttrSet:
                                 # Competitive propensities
                                 if (src_val[0], trg_val[0]) in compPairs or (trg_val[0],src_val[0]) in compPairs:
                                 # Checking to see if each node's attribute weights fall within specified ranges:
                                     if src_w < 0.6 and trg_w < 0.6:
-                                        emoProps.append(("Surprise", attr, src_val[0], trg_val[0],src_w,trg_w))
+                                        emoProps.append(("Surprise", attr, src_val[0], trg_val[0],src_w,trg_w,src_w*trg_w))
+                                        inflProps += [
+                                            ("Reciprocation", attr, src_val[0], trg_val[0], src_w, trg_w, 0.4),
+                                            ("Commitment & Consistency", attr, src_val[0], trg_val[0], src_w, trg_w,
+                                             0.2),
+                                        ]
                                     else:
-                                        emoProps.append(("Anticipation", attr, src_val[0], trg_val[0],src_w,trg_w))
+                                        emoProps.append(("Anticipation", attr, src_val[0], trg_val[0],src_w,trg_w,src_w*trg_w))
+                                        inflProps += [
+                                            ("Reciprocation", attr, src_val[0], trg_val[0], src_w, trg_w, 0.5),
+                                            ("Commitment & Consistency", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                            ("Social Proof", attr, src_val[0], trg_val[0], src_w, trg_w, 0.5),
+                                            ("Liking", attr, src_val[0], trg_val[0], src_w, trg_w, 0.5),
+                                            ("Authority", attr, src_val[0], trg_val[0], src_w, trg_w, 0.7),
+                                            ("Scarcity", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                        ]
 
                                 # Coercive propensities:
                                 elif (src_val[0], trg_val[0]) in oppPairs or (trg_val[0],src_val[0]) in oppPairs:
                                     # Checking to see if each node's attribute weights fall within specified ranges:
                                     if (src_w >= 0.8 and trg_w >= 0.6) or (src_w >= 0.6 and trg_w >= 0.6):
-                                        emoProps.append(("Anger", attr, src_val[0], trg_val[0],src_w,trg_w))
+                                        emoProps.append(("Anger", attr, src_val[0], trg_val[0],src_w,trg_w,src_w*trg_w))
+                                        inflProps += [
+                                            ("Reciprocation", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Commitment & Consistency", attr, src_val[0], trg_val[0], src_w, trg_w,
+                                             0.2),
+                                            ("Social Proof", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Liking", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Authority", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Scarcity", attr, src_val[0], trg_val[0], src_w, trg_w, 0.5),
+                                        ]
                                     elif (src_w >= 0.6 and trg_w >= 0.4) or (src_w >= 0.4 and trg_w >= 0.6):
-                                        emoProps.append(("Sadness", attr, src_val[0], trg_val[0],src_w,trg_w))
+                                        emoProps.append(("Sadness", attr, src_val[0], trg_val[0],src_w,trg_w,src_w*trg_w))
+                                        inflProps += [
+                                            ("Social Proof", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Liking", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Authority", attr, src_val[0], trg_val[0], src_w, trg_w, 0.5),
+                                        ]
                                     elif (src_w >= 0.4 and trg_w >= 0.2) or (src_w >= 0.2 and trg_w >= 0.4):
-                                        emoProps.append(("Fear", attr, src_val[0], trg_val[0],src_w,trg_w))
+                                        emoProps.append(("Fear", attr, src_val[0], trg_val[0],src_w,trg_w,src_w*trg_w))
+                                        inflProps += [
+                                            ("Reciprocation", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                            ("Commitment & Consistency", attr, src_val[0], trg_val[0], src_w, trg_w,
+                                             0.8),
+                                            ("Social Proof", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                            ("Liking", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Authority", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
+                                            ("Scarcity", attr, src_val[0], trg_val[0], src_w, trg_w, 0.5),
+                                        ]
                                     elif (src_w >= 0.8 and trg_w >= 0.2) or (src_w >= 0.2 and trg_w >= 0.8):
-                                        emoProps.append(("Disgust", attr, src_val[0], trg_val[0],src_w,trg_w))
+                                        emoProps.append(("Disgust", attr, src_val[0], trg_val[0],src_w,trg_w,src_w*trg_w))
+                                        inflProps += [
+                                            ("Reciprocation", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Commitment & Consistency", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Social Proof", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Liking", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Authority", attr, src_val[0], trg_val[0], src_w, trg_w, 0.2),
+                                            ("Scarcity", attr, src_val[0], trg_val[0], src_w, trg_w, 0.4),
+                                        ]
 
-                        ## Role Propensities
+                        ## Role Propensities ##
+
                         # Still need to add conditional opposites, like in emotion
 
                         src_amt = float(src_val[1]["AMT"]) if attr == "Resource" and "AMT" in src_val[1] else None
