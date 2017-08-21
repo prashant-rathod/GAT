f398ed25399b61ed45c5df9fc3e3dae8fd76ab9e
rsteed11 <rsteed11@gmail.com>
2017-08-21 12:11:45 -0400

    Added role propensities prototype

--- gat/core/sna/propensities.py
+++ gat/core/sna/propensities.py
@@ -2,11 +2,12 @@ def propCalc(graph, edge):
     emoProps = []
     roleProps = []
     inflProps = []
-    roleAttrSet = ["Belief", "Resource"]
+    roleAttrSet = ["Belief","Resource"]
     source = graph.G.node[edge[0]]
     target = graph.G.node[edge[1]]
-    # Check if role attribute is present; if not, no role propensities calculated
-    roleFlag = True if source.get("Role") is not None and target.get("Role") is not None else False
+    src_role = source.get("Role")
+    trg_role = target.get("Role")
+
     for attr in (target if len(source) > len(target) else source):
         if attr not in ['block', 'newNode', 'Name'] and source.get(attr) is not None and target.get(attr) is not None:
             for src_val in [x for x in source.get(attr) if len(x) > 1]:
@@ -16,13 +17,34 @@ def propCalc(graph, edge):
                     ### Propensity assignment section ###
                     #####################################
 
-                    ## Emotion & Influence Propensities ##
+                    ### Emotion & Influence Propensities ###
                     src_w = float(src_val[1]["W"]) if "W" in src_val[1] else None
                     trg_w = float(trg_val[1]["W"]) if "W" in trg_val[1] else None
-                    if src_w is not None and trg_w is not None:
-                        index_w = src_w * trg_w # index weight: product of src and trg weights, used to evaluate propensities
-                        # Cooperative propensities:
-                        if src_val[0] == trg_val[0]:
+
+                    ### Relationship Categories for Roles ###
+                    # Check if role attribute is present; if not, no role propensities calculated
+                    roleRelation = 0 if source.get("Role") is not None and target.get("Role") is not None else None
+                    if roleRelation == 0 and attr in roleAttrSet:
+                        ## Calculate relationship type: 0 for neutral, 1 for friend, -1 for adversary
+                        # Friends
+                        if src_role == "Developer" and trg_role == "Developer":
+                            roleRelation += 1
+                        if src_role not in ["Model","Defender of the Faith","Nonaffiliated Independent","Mediator"] and trg_role in ["Ally","Supporter","Dependent"]:
+                            roleRelation += 1
+                        # Adversary
+                        if src_role == "Hegemon" and trg_role == "Revisionist" or src_role == "Revisionist" and trg_role == "Hegemon":
+                            roleRelation -= 1
+                        # If a hegemon/revisionist is a neighbor of the target node, adversarial
+                        if src_role == "Hegemon" and "Revisionist" in [neighbor.get("Role") for neighbor in graph[edge[0]]] or src_role == "Revisionist" and "Hegemon" in [neighbor.get("Role") for neighbor in graph[edge[0]]]:
+                            roleRelation -= 1
+                        # Could be using resources, but not yet
+                        # src_amt = float(src_val[1]["AMT"]) if attr == "Resource" and "AMT" in src_val[1] else None
+                        # trg_amt = float(trg_val[1]["AMT"]) if attr == "Resource" and "AMT" in trg_val[1] else None
+
+                    # Cooperative propensities
+                    if src_val[0] == trg_val[0]:
+                        index_w = src_w * trg_w if src_w is not None and trg_w is not None else None
+                        if index_w is not None:
                             # Checking to see if each combo's attribute weight index fall within specified ranges:
                             if index_w > 0.36:
                                 emoProps.append(("Trust", attr, src_val[0], trg_val[0], src_w, trg_w, index_w))
@@ -65,9 +87,14 @@ def propCalc(graph, edge):
                                     ("Authority", attr, src_val[0], trg_val[0], src_w, trg_w, 0.7),
                                     ("Scarcity", attr, src_val[0], trg_val[0], src_w, trg_w, 0.8),
                                 ]
+                        ## Role Propensities ##
+                        if roleRelation is not None and roleRelation > -1:
+                            roleProps.append(("Consumer-Provider Relationship", attr, src_val[0], trg_val[0], src_w, trg_w, index_w) if attr == "Resource" else None)
+                            roleProps.append(("Protector", attr, src_val[0], trg_val[0], src_w, trg_w, index_w) if attr == "Belief" else None)
 
-                        # Coercive propensities:
-                        else:
+                    # Coercive and competitive propensities:
+                    else:
+                        if index_w is not None:
                             # Checking to see if each node's attribute weights fall within specified ranges:
                             if index_w > 0.62:
                                 emoProps.append(
@@ -108,18 +135,8 @@ def propCalc(graph, edge):
                                     ("Commitment & Consistency", attr, src_val[0], trg_val[0], src_w, trg_w,
                                      0.2),
                                 ]
-
-                    ## Role Propensities ##
-                    # Still need to add conditional opposites, like in emotion
-                    src_amt = float(src_val[1]["AMT"]) if attr == "Resource" and "AMT" in src_val[1] else None
-                    trg_amt = float(trg_val[1]["AMT"]) if attr == "Resource" and "AMT" in trg_val[1] else None
-                    if roleFlag and attr in roleAttrSet:
-                        if src_val[0] == trg_val[0]:
-                            roleProps.append(["Consumer or Provider", .5] if attr == "Resource" else None)
-                            # print("Appended Cons. or Prov. using attribute", attr, "(", src_val, "&", trg_val, ")",
-                            #       "for node pair (", source, ",", target, ")")
-                            roleProps.append(["Protector", .75] if attr == "Belief" else None)
-                            # print("Appended Protector using attribute", attr, "(", src_val, "&", trg_val, ")",
-                            #       "for node pair (", source, ",", target, ")")
-
+                        ## Role Propensities ##
+                        if roleRelation is not None and roleRelation < 0:
+                            roleProps.append(("Belligerent", attr, src_val[0], trg_val[0], src_w, trg_w, index_w) if attr == "Belief" else None)
+    if len(roleProps) > 0: print(roleProps)
     return emoProps, roleProps, inflProps
