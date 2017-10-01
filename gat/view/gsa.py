369964825b68a2cb073323e3b879cc4ca3f5da58
Nikita Zemlevskiy <naz7@duke.edu>
2017-10-01 18:32:51 -0400

    enable showing locations which are present in shapefile

--- gat/view/gsa.py
+++ gat/view/gsa.py
@@ -4,8 +4,8 @@ import ast
 
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 from gat.core.gsa.misc import util
-import xlrd
 from gat.service import gsa_service
+from gat.service import io_service
 
 from gat.core.gsa.core import weights, regionalization
 from gat.dao import dao
@@ -27,31 +27,17 @@ def gsa_select():
         fileDict['GSA_Input_CSV'] = url_for('static', filename="sample/gsa/" + csv)[1:]
         fileDict['GSA_Input_SHP'] = url_for('static', filename="sample/gsa/" + shp)[1:]
 
-    '''
-    if sample_path == "usa":
-        localAutoCorrelation, globalAutoCorrelation, spatialDynamics = gsa_service.runGSA(case_num, "ALL", [2008],
-                                                                                          "ALL",
-                                                                                          list(range(1979, 2009)),
-                                                                                          "STATE_NAME")
-        fileDict['GSA_data'] = ('id-1', localAutoCorrelation, globalAutoCorrelation,
-                                spatialDynamics[0], spatialDynamics[1], spatialDynamics[2], spatialDynamics[3])
-        fileDict['GSA_meta'] = (
-            'data-state-id', 'data-state-name', "STATE_NAME", list(range(1979, 2009)), "state-name")
-    else:
-    '''
-
     # TODO: take in years and file names instead of hard coding
     # TODO: reorganize use of gsa_meta
 
     class Input:
-        def __init__(self, autoRow, autoCol, dynRow, dynCol, id):
+        def __init__(self, autoRow, autoCol, dynRow, dynCol):
             self.autoRow = autoRow
             self.autoCol = autoCol
             self.dynRow = dynRow
             self.dynCol = dynCol
-            self.id = id
 
-    info = Input("ALL", ["2014.0"], "ALL", np.arange(2014, 2017, 0.25).tolist(), "NAME_1")
+    info = Input("ALL", ["2014.0"], "ALL", np.arange(2014, 2017, 0.25).tolist())
 
     if request.method == 'GET':
         return render_template("gsaselect.html", info=info, case_num=case_num)
@@ -63,14 +49,15 @@ def gsa_select():
         info.dynRow = request.form.get('dyn-row')
         #info.dynCol = ast.literal_eval(request.form.get('dyn-col'))
         info.id = request.form.get('gsa-id')
+        id = fileDict['GSA_SHP_VARS'][0]
 
         localAutoCorrelation, globalAutoCorrelation, spatialDynamics = gsa_service.runGSA(case_num, info.autoRow,
                                                                                           info.autoCol, info.dynRow,
-                                                                                          info.dynCol, info.id)
+                                                                                          info.dynCol, id)
 
         fileDict['GSA_data'] = ('id-1', localAutoCorrelation, globalAutoCorrelation,
                                 spatialDynamics[0], spatialDynamics[1], spatialDynamics[2], spatialDynamics[3])
-        fileDict['GSA_meta'] = ('data-id-1', 'data-name-1', "NAME_1", np.arange(2014, 2017, 0.25).tolist(), "name-1")
+        fileDict['GSA_meta'] = ('data-id-1', 'data-name-1', "NAME_1", np.arange(2014, 2017, 0.25).tolist(), fileDict['GSA_SHP_VARS'][1])
 
         return redirect(url_for('visualize_blueprint.visualize', case_num=case_num))
 
@@ -116,3 +103,32 @@ def get_autocorrelation(case_num):
         loc, glob = fileDict['ac'][year]
         return jsonify(year=year, loc=loc, glob=glob)
     return jsonify(year="something went wrong", loc=0, glob=0)
+
+@gsa_blueprint.route("/_gsa_csv", methods = ['GET'])
+def upload_csv_get():
+    case_num = request.args.get('case_num', None)
+    fileDict = dao.getFileDict(case_num)
+    GSA_file_SHP = fileDict.get('GSA_Input_SHP')
+    gsaSVG = "out/gsa/mymap.svg"
+    gsa_service.generateMap(GSA_file_SHP, gsaSVG)
+    nameMapping = gsa_service.getNameMapping(gsaSVG, fileDict['GSA_SHP_VARS'][1])
+    return render_template("gsaUploadCsv.html", names = nameMapping)
+
+@gsa_blueprint.route("/_gsa_csv", methods = ['POST'])
+def upload_csv_post():
+    case_num = request.args.get('case_num', None)
+    fileDict = dao.getFileDict(case_num)
+    #TODO: check for .csv extention error
+    fileDict['GSA_Input_CSV'] = io_service.storefile(request.files.get('GSA_Input_CSV'))
+    return redirect(url_for("gsa_blueprint.gsa_select", case_num = case_num))
+
+@gsa_blueprint.route("/_gsa_vars", methods = ['GET'])
+def shp_vars_get():
+    return render_template("gsaVars.html", id = "NAME_1", name_var = "data-name-1")
+
+@gsa_blueprint.route("/_gsa_vars", methods=['POST'])
+def shp_vars_post():
+    case_num = request.args.get('case_num', None)
+    fileDict = dao.getFileDict(case_num)
+    fileDict['GSA_SHP_VARS'] = [request.form.get('gsa-id'), request.form.get('gsa-name-var')]
+    return redirect(url_for("gsa_blueprint.upload_csv_get", case_num = case_num))
--- gat/view/upload.py
+++ gat/view/upload.py
@@ -25,7 +25,6 @@ def upload():
     # here the use of fileDict is probably more clear
     # the strings used to index request.files come from the HTML name of the input field
     # see upload.html
-    fileDict['GSA_Input_CSV'] = io_service.storefile(request.files.get('GSA_Input_CSV'))
     fileDict['GSA_Input_SHP'] = io_service.storeGSA(request.files.getlist('GSA_Input_map'))
     fileDict['GSA_file_list'] = request.files.getlist('GSA_Input_map')
 
@@ -50,8 +49,8 @@ def upload():
     if fileDict['SNA_Input']:
         return redirect(url_for('sna_blueprint.sheetSelect', case_num=case_num))
 
-    if fileDict['GSA_Input_CSV']:
-        return redirect(url_for('gsa_blueprint.gsa_select', case_num=case_num))
+    if fileDict['GSA_Input_SHP']:
+        return redirect(url_for('gsa_blueprint.shp_vars_get', case_num=case_num))
 
     # if a user does both SNA and NLP, as it stands, the NLP intermediary data will never be gotten to. This is a problem.
     if fileDict['NLP_Input_corpus']:
