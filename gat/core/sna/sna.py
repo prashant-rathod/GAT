cafddf460401b59b683830bfd865df4bd2f42285
Nikita Zemlevskiy <naz7@duke.edu>
2017-11-02 10:36:28 -0400

    fix sna events issue

--- gat/core/sna/sna.py
+++ gat/core/sna/sna.py
@@ -301,9 +301,10 @@ class SNA():
         dateList = [bombData[x]['Date'] for x in bombData]
         dateIter = (max(dateList) - min(dateList)) / 10
         output_dict = {}
+        ret = {}
         for i in range(max_iter):
             nodeList = [(bombData[x]['Actor'], bombData[x]['Target'], bombData[x]['CODE']) for x in bombData if
-                          min(dateList) + dateIter * i <= bombData[x]['Date'] < min(dateList) + dateIter * (i+1)]
+                         min(dateList) + dateIter * i <= bombData[x]['Date'] < min(dateList) + dateIter * (i + 1)]
             # adding attacks to test graph by datetime period and iterating through to change sentiments
             iterEdgeList = []
             for node in nodeList:
@@ -311,54 +312,87 @@ class SNA():
                     # rejection of source
                     # if self.G.has_edge(node[0], others) or self.G.has_edge(node[1], others):
                     for ontClass in self.classList:
-                        sent = self.G.node[others].get(ontClass) # the attribute, if it exists
+                        sent = self.G.node[others].get(ontClass)  # the attribute, if it exists
                         if sent is not None:
                             for item in [item for item in sent if len(item) == 2]:
+                                original_output = float(item[1]['W'])
                                 if item[1].get('W') is not None:
                                     if item[0] == node[0]:
                                         original = float(item[1]['W'])
-                                        item[1]['W'] = original * 0.9
+
+                                        item[1]['W'] = original * 0.99
                                         output_dict[node[0] + " towards " + others] = item[1]['W'] - original
+                                        # output_dict[node[0] + " towards " + others + " (previous) "] = original_output
                                     if item[0] == node[1]:
                                         original = float(item[1]['W'])
-                                        item[1]['W'] = original * 1.1
-                                        output_dict[node[1] + " towards " + others] = item[1]['W'] - original
 
-                                    # sympathy for city population - HARDCODED
+                                        item[1]['W'] = original * 1.05
+                                        output_dict[node[1] + " towards " + others] = item[1]['W'] - original
+                                        # output_dict[node[1] + " towards " + others + " (previous) "] = original_output
+
+                                    # response of city populations - HARDCODED
                                     if item[0] == "Shi'ism" and float(item[1]['W']) > -0.5:
                                         if node[1] == 'Najaf':
                                             original = float(item[1]['W'])
-                                            item[1]['W'] = original * 1.1
+                                            item[1]['W'] = original * 1.05
                                             output_dict[node[1] + " towards " + others] = item[1]['W'] - original
+                                            # output_dict[node[1] + " towards " + others + " (previous) "] = original_output
                                         if node[1] == 'Basra':
                                             original = float(item[1]['W'])
-                                            item[1]['W'] = original * 1.1
+                                            item[1]['W'] = original * 1.05
                                             output_dict[node[1] + " towards " + others] = item[1]['W'] - original
+                                            # output_dict[node[1] + " towards " + others + " (previous) "] = original_output
                                     if item[0] == "Kurdish Nationalism" and float(item[1]['W']) > -0.5:
                                         if node[1] == 'Kirkuk':
                                             original = float(item[1]['W'])
-                                            item[1]['W'] = original * 1.1
+                                            item[1]['W'] = original * 1.05
                                             output_dict[node[1] + " towards " + others] = item[1]['W'] - original
+                                            # output_dict[node[1] + " towards " + others + " (previous) "] = original_output
                                     if item[0] == "Sunni'ism" and float(item[1]['W']) > -0.5:
                                         if node[1] == 'Fallujah':
                                             original = float(item[1]['W'])
-                                            item[1]['W'] = original * 1.1
+                                            item[1]['W'] = original * 1.05
                                             output_dict[node[1] + " towards " + others] = item[1]['W'] - original
+                                            # output_dict[node[1] + " towards " + others + " (previous) "] = original_output
                                     if others == 'ISIL_al-Baghdadi':
                                         original = float(item[1]['W'])
-                                        item[1]['W'] = original * 0.9
+                                        item[1]['W'] = original * 0.99
                                         output_dict[node[1] + " towards " + others] = item[1]['W'] - original
+                                        # output_dict[node[1] + " towards " + others + " (previous) "] = original_output
+
                 # add an event node
-                event = 'Event '+str(node[2])+': '+node[0]+' to '+node[1]
-                self.G.add_node(event, {'ontClass':'Event', 'block':'Event', 'Name':['Event'+str(i)+' '+str(node[2])+': '+node[0]+' to '+node[1]], 'block':'Event','Description': 'Conduct suicide, car, or other non-military bombing'})
+                event = 'Event ' + str(node[2]) + ': ' + node[0] + ' to ' + node[1]
+                self.G.add_node(event, {'ontClass': 'Event', 'Name': [
+                    'Event' + str(i) + ' ' + str(node[2]) + ': ' + node[0] + ' to ' + node[1]], 'block': 'Event',
+                                        'Description': 'Conduct suicide, car, or other non-military bombing'})
                 self.G.add_edge(node[0], event)
                 self.G.add_edge(event, node[1])
             self.G.add_weighted_edges_from(iterEdgeList, 'W')
 
+            ### -1 TO 1 SCALING METHODS ###
+
+            # division by largest value -1 to 1 scaling: (works best with large numbers)
+            # for k, v in output_dict.items():
+            #    abs_list = [abs(n) for n in list(output_dict.values())]
+            #    output_dict[k] = round((v / max(abs_list)), 5)
+
+            # max and min cut off -1 to 1 scaling: (if there are too many 1 or -1 results...
+            # ...the multipliers (n) in <item[1]['W'] = original * n> are too divergent from 1
+            for k, v in output_dict.items():
+                output_dict[k] = round(v, 5)
+                if v > 1:
+                    output_dict[k] = 1
+                if v < -1:
+                    output_dict[k] = -1
+
+            # this print statement needs to be where out_put dict is printed after each iteration:
+            #print(output_dict)
+            ret[i] = output_dict.copy()
+
         self.nodes = nx.nodes(self.G)  # update node list
         self.edges = nx.edges(self.G)  # update edge list
         self.output_dict.update(output_dict)
-        return self.output_dict
+        return ret
 
     # copy the original social network graph created with user input data.
     # this will be later used to reset the modified graph to inital state
