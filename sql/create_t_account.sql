f7a8abc26476c866390c550cf179529f5d4012dd
Nikita Zemlevskiy <naz7@duke.edu>
2017-11-03 19:15:44 -0400

    update create_t_account.sql

--- sql/create_t_account.sql
+++ sql/create_t_account.sql
@@ -1,5 +1,7 @@
 CREATE TABLE t_account (
     uidpk           integer PRIMARY KEY,
     email           varchar(40),
-    password_hash   varchar(128)
+    password_hash   varchar(128),
+    confirmed       boolean,
+    confirmation_string varchar(30)
 );
