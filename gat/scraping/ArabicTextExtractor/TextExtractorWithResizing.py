451584ecf5307e42ef4d4724bc9dd477f524d248
Suchir <suchir.bhatt@gmail.com>
2017-11-10 17:14:10 -0500

    Input pipeline

--- gat/scraping/ArabicTextExtractor/TextExtractorWithResizing.py
+++ gat/scraping/ArabicTextExtractor/TextExtractorWithResizing.py
@@ -9,15 +9,13 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from spp.SpatialPyramidPooling import SpatialPyramidPooling
 from keras import backend as K
 import scipy
 
 
-#So from here hopefully the one hot is done, now the labels will have to be transformed and then use the .fit() method to train the network
-#Potentially need to make the image reading better
-
+#42 unique codes for arabic characters
 #Up to 125 characters per line
+#So do 43 codes (one for empty) and then have a 43 row softmax output with 125 columns?
 #Potential problem, all the pictures with whitespace at the top?
 
 def clean_line(line):
@@ -35,59 +33,52 @@ def get_labels(path):
         line_dict[image_name] = temp_array[1:]
     return line_dict
 
-def create_one_hot(labels):
-    done_labels = {}
-    for label in labels:
-        if label not in done_labels:
-            done_labels[label] = len(done_labels)
-    return done_labels
-
 def get_batch(image_dir, label_dict, batch_size, width, height, channels):
-    image_batch = []
+    image_batch = np.empty((batch_size,width,height,channels))
     label_batch = []
     for i in range(batch_size):
         image_name = random.choice(os.listdir(image_dir))
         img = load_img(image_dir+'/'+image_name) # this is a PIL image
         x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
-        image_batch.append(x)
+        x = scipy.misc.imresize(x, (width,height,channels)) # this is a Numpy array with shape (1, 3, 150, 150)
+        print(x.shape)
+        image_batch[i,:,:,:] = x
         label_batch.append(label_dict[image_name[0:image_name.find(".")]])
+    print(image_batch.shape)
     return image_batch, label_batch
-
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(None, None, 3)))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(SpatialPyramidPooling([1, 2, 4]))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(43, activation='softmax'))
-model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.adam(),
-              metrics=['accuracy'])
 
 image_dir = "/home/cheesecake/Desktop/KHATT_v1.0/LineImages_v1.0/FixedTextLineImages/Train"
 label_path = '/home/cheesecake/GAT/gat/scraping/ArabicTextExtractor/FixedTextLinesLatinTransliteration/TrainLabels_Translated.txt'
 height,width,channels = 256,256,3
-batch_size = 16
 
-label_list = get_labels(label_path).values()
-all_labels = []
-for i in label_list:
-    for j in i:
-        all_labels.append(j)
 
-one_hot_dict = create_one_hot(all_labels)
-one_hot_dict[''] = len(one_hot_dict)
-print(one_hot_dict)
+image_batch, label_batch = get_batch(image_dir,get_labels(label_path), 16, width,height,channels)
+for i in range(16):
+    image = image_batch[i,:,:,:]
+    image = image.reshape(width,height,channels)
+    plt.imshow(image)
+    plt.show()
 
-for i in range(1000):
-    image_batch, label_batch = get_batch(image_dir, get_labels(label_path), batch_size, width, height, channels)
-    for i in range(0):
-        image = image_batch[i]
-        plt.imshow(image)
-        plt.show()
+image_batch = image_batch.astype('float32')
+image_batch /= 255
+print('x_train shape:', image_batch.shape)
+print(image_batch.shape[0], 'train samples')
 
-    for i in range(batch_size):
-        image_batch[i] = image_batch[i].astype('float32')
-        image_batch[i] = image_batch[i] / 255
+# convert class vectors to binary class matrices
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(SpatialPyramidPooling([1, 2, 4]))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(43, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.adam(),
+              metrics=['accuracy'])
